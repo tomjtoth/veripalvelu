7048e482c6d0de91e719cd0dfd363e035bc3e225
Tamás Tóth <tomjtoth@gmail.com>
2023-10-16 20:46:08 +0300

    refined esaster-egg in donate.html

--- static/donation.js
+++ static/donation.js
@@ -14,5 +14,10 @@ class Donation {
                 ? `(${5000 - value.length} merkkiä tilaa jäljellä)`
                 : '';
         });
+
+        document.addEventListener('click', ({ target: { classList, tagName } }) => {
+            if (classList.contains('easter-egg') && tagName == 'SPAN')
+                Fun.spam(100);
+        });
     }
 }
