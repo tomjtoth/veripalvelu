3f5587449d77c3af4145a74b86b2f761643bd5e5
Tamás Tóth <tomjtoth@gmail.com>
2023-10-22 10:49:59 +0300

    donation volume lowered

--- static/donation.js
+++ static/donation.js
@@ -13,7 +13,7 @@ class Donation {
             this.dates = dates;
         });
 
-        this._snd.volume = 0.2;
+        this._snd.volume = 0.05;
         this._snd.onended = () => {
             this._submit_target.dispatchEvent(new Event('submit'));
         }
