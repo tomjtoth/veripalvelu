1968735e018a65e6bae3d7176f912caf280fb835
Tamás Tóth <tomjtoth@gmail.com>
2024-03-20 09:43:19 +0200

    removed `.env` creation from `app.py`

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.12-alpine3.19
 
-WORKDIR /usr/src/app/
+WORKDIR /usr/src/app
 
 COPY ./requirements.txt .
 ENV VIRTUAL_ENV=/opt/venv
@@ -9,13 +9,9 @@ ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 RUN pip install -r requirements.txt
 
 # the above block takes long, requirements don't change as often as the /src/
-VOLUME .env
 COPY . .
 
 RUN adduser --no-create-home --disabled-password --gecos "" veripalvelu
-RUN chown veripalvelu:veripalvelu .
 USER veripalvelu
-
-RUN CREATE_DOTENV=true python3 app.py
 
 ENTRYPOINT gunicorn --bind 0.0.0.0 app:app
--- app.py
+++ app.py
@@ -2,31 +2,10 @@
 
 responsible for:
 - reading in command line arguments
-- creating the `.env` preset
 """
 
-from os import getenv, path, environ
-import secrets
-from subprocess import CalledProcessError, run
-import sys
-from textwrap import dedent
-import uuid
+from os import getenv
 from flask import Flask
-
-if getenv("CREATE_DOTENV", "false") == "true":
-    POSTGRES_PASSWORD = str(uuid.uuid4())
-    with open(".env", "a", encoding="utf8") as f:
-        f.write(dedent(f"""
-        # needed for Flask
-        SERCET_KEY={str(uuid.uuid4())}
-        
-        # needed for docker-compose, too
-        POSTGRES_PASSWORD={POSTGRES_PASSWORD}
-        
-        # this might need to be adjusted
-        DATABASE_URL=postgresql+psycopg2://vp:{POSTGRES_PASSWORD}@localhost/vp
-        """))
-    sys.exit()
 
 
 app = Flask(__name__)
